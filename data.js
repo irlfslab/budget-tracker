d6aab85cf7edb0a48109f9473af7f103b25382a3
IL <53288945+irlfslab@users.noreply.github.com>
2024-07-11 23:42:42 -0400

    Add files via upload

---
+++ data.js
@@ -0,0 +1,6 @@
+const trans = [
+    {id: 123, source: "Cashier", amount: 600, time: "4:50:38 AM 5/19/2024"},
+    {id: 124, source: "Studio", amount: -50, time: "5:50:38 AM 5/20/2024"},
+    {id: 125, source: "Cash", amount: 500, time: "7:45:38 AM 5/21/2024"},
+    {id: 126, source: "QA", amount: 200, time: "9:48:38 AM 5/22/2024"},
+];
